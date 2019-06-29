273fb81b995bd5f51b862625f8e00131a1f25ba2
sa6o sa6o <aleksandarboev95@gmail.com>
2019-06-29 19:27:38 +0300

    Finished tasks

--- 04.dom-manipulations-exercise/tasks/t08-encode-and-decode-messages/app.js
+++ 04.dom-manipulations-exercise/tasks/t08-encode-and-decode-messages/app.js
@@ -1,3 +1,52 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const changeCharCode = (character, amount) => String.fromCharCode(character.charCodeAt(0) + amount);
+
+    /**
+     * @param string {String} String on which characters will be changed by the function
+     * @param characterChangeFunction {Function} Single param function, which will be applied to each character
+     * @returns {string|string} Mutated string
+     */
+    const getChangedString = (string, characterChangeFunction) => {
+        let result = '';
+        for (let i = 0; i < string.length; i += 1) {
+            result += characterChangeFunction(string[i]);
+        }
+
+        return result;
+    };
+
+    //Using the "Partial Application" trick.
+    //Create new functions which do the same things, just with a fixed second param.
+    const encoderFunction = character => changeCharCode(character, 1);
+    const decoderFunction = character => changeCharCode(character, -1);
+
+    const encodeAndDecodeDivElements = document.querySelector('main#main').children;
+    const encodeDivElement = encodeAndDecodeDivElements[0];
+    const decodeDivElement = encodeAndDecodeDivElements[1];
+
+    const cypherElement = (divElement) => {
+        const textAreaElement = divElement.querySelector('textarea');
+        const buttonElement = divElement.querySelector('button');
+
+        return {
+            getTextAreaElement: textAreaElement,
+            getButtonElement: buttonElement,
+        }
+    };
+
+    const encodeCypher = cypherElement(encodeDivElement);
+    const decodeCypher = cypherElement(decodeDivElement);
+
+    encodeCypher.getButtonElement.addEventListener('click', () => {
+        const messageToBeEncoded = encodeCypher.getTextAreaElement.value;
+        encodeCypher.getTextAreaElement.value = '';
+        const encodedMessage = getChangedString(messageToBeEncoded, encoderFunction);
+        decodeCypher.getTextAreaElement.value = encodedMessage;
+    });
+
+    decodeCypher.getButtonElement.addEventListener('click', () => {
+        const messageToBeDecoded = decodeCypher.getTextAreaElement.value;
+        const decodedMessage = getChangedString(messageToBeDecoded, decoderFunction);
+        decodeCypher.getTextAreaElement.value = decodedMessage;
+    });
 }
--- 04.dom-manipulations-exercise/tasks/t09-distance-converter/distanceConverter.js
+++ 04.dom-manipulations-exercise/tasks/t09-distance-converter/distanceConverter.js
@@ -1,3 +1,45 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    /**
+     * Works with: "km" (Kilometers), "m" (Meters), "cm" (Centimeters), "mm" (Millimeters),
+     * "mi" (Miles), "yrd" (Yards), "ft" (Feet), "in" (Inches)
+     * @param distance {Number}
+     * @param from {String}
+     * @param to {String}
+     */
+    const convertDistance = (distance, from, to) => {
+        if (distance === 0) {
+            return 0;
+        }
+
+        const toMeters = {
+            m: 1,
+            km: 1000,
+            cm: 0.01,
+            nn: 0.001,
+            mi: 1609.34,
+            yrd: 0.9144,
+            ft: 0.3048,
+            in: 0.0254,
+        };
+
+        const amountInMeters = distance * toMeters[from];
+        return amountInMeters / toMeters[to];
+    };
+
+    const convertButtonElement = document.querySelector('#convert');
+
+    const inputUnitsSelectElement = document.querySelector('#inputUnits');
+    const inputDistanceInputElement = document.querySelector('#inputDistance');
+
+    const outputDistanceInputElement = document.querySelector('#outputDistance');
+    const outputUnitsSelectElement = document.querySelector('#outputUnits');
+
+    convertButtonElement.addEventListener('click', () => {
+        const convertFrom = inputUnitsSelectElement[inputUnitsSelectElement.selectedIndex].value;
+        const convertTo = outputUnitsSelectElement[outputUnitsSelectElement.selectedIndex].value;
+
+        const result = convertDistance(Number(inputDistanceInputElement.value), convertFrom, convertTo);
+        outputDistanceInputElement.value = result;
+    })
 }
+
